4130bde842dabfed6721a1cb427ec05af070a155
Abhishek <abhishekkumar199671@gmail.com>
2026-02-16 00:04:31 +0530

    updated profile

--- chat-app/src/pages/ProfileUpdate/Profile.jsx
+++ chat-app/src/pages/ProfileUpdate/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Profile.css'
+import assets from '../../assets/assets'
 const Profile = () => {
   return (
     <div className='profile'>
@@ -7,9 +8,15 @@ const Profile = () => {
         <form>
           <h3>Profile Details</h3>
           <label htmlFor='avatar'>
-            <input type='file' id='avatar'/>
+            <input type='file' id='avatar' accept='.png , .jpg , .jpeg' hidden/>
+            <img src={assets.avatar_icon} alt=''/>
+            Upload profile image
           </label>
+          <input type='text' placeholder='Your name' required/>
+          <textarea placeholder='Write profile bio' required></textarea>
+          <button type='submit'>Save</button>
         </form>
+        <img src={assets.logo_icon} alt=""/>
       </div>
     </div>
   )
